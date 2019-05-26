ee6cf7a732b48f0869cae988e939d7d98b27534c
shige <ts765125@yahoo.co.jp>
2019-05-26 09:56:52 +0900

    update postjson.py

--- gcp-project/postjson.py
+++ gcp-project/postjson.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'https://sensor-shige-test.appspot.com/'
+url = 'https://〇〇◯.appspot.com/'
 payload = {'some': 'data'}
 headers = {'content-type': 'application/json'}
 r = requests.post(url, data=json.dumps(payload), headers=headers)
